af931f91a047beab612dbbd463f85a5bb9f11bca
Monish <monish635112@gmail.com>
2025-09-25 21:31:07 +0530

    Find all products that are in the 'Electronics' category.

--- Exercise Set 2.sql
+++ Exercise Set 2.sql
@@ -2,3 +2,7 @@
 --Find the product with a product_id of 5.
 SELECT product_name FROM products
 WHERE product_id = 5;
+
+--Find all products that are in the 'Electronics' category.
+SELECT * FROM products
+WHERE category = 'Electronics';
