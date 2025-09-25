2b2c35ca013077c139eb1498e024368eba4337b9
Monish <monish635112@gmail.com>
2025-09-25 21:08:20 +0530

    Exercise Set 1: Fetching and Sorting Data (SELECT, ORDER BY)

---
+++ Exercise Set 1.sql
@@ -0,0 +1,6 @@
+--Exercise Set 1: Fetching and Sorting Data (SELECT, ORDER BY)
+--Select all information for every product in the table.
+SELECT * FROM products;
+
+--Select just the name and price of every product.
+SELECT product_name , price FROM products;
