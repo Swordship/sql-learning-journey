5e6f3cc64db5a7c94f7d3a0efa6b6e4dd6cd9d2b
Monish <monish635112@gmail.com>
2025-09-26 09:58:21 +0530

    Find all products that are in the 'Home Goods' category OR the 'Furniture' category.

--- Exercise Set 3.sql
+++ Exercise Set 3.sql
@@ -2,3 +2,7 @@
 --Find all products that are in the 'Electronics' category AND have a price less than $1000.
 SELECT * FROM products
 WHERE category = 'Electronics' AND price < 1000.00;
+
+--Find all products that are in the 'Home Goods' category OR the 'Furniture' category.
+SELECT * FROM products
+WHERE category = 'Home Goods' OR category = 'Furniture';
