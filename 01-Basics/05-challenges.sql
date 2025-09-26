b2abaa7595e76e3bd739545e630e0cf0acc3316e
Monish <monish635112@gmail.com>
2025-09-26 21:47:41 +0530

    do a folder and file name

--- 01-Basics/05-challenges.sql
+++ 01-Basics/05-challenges.sql
@@ -1,3 +1,11 @@
+--Challenge 1: Find all products in the 'Accessories' category that cost more than $50, and 
+--list them with the most expensive ones first.
+SELECT * FROM products;
+
+SELECT * FROM products
+WHERE category = 'Accessories' AND price > 50.00
+ORDER BY price DESC;
+
 --Challenge 2: Find all 'Electronics' or 'Accessories' that are in stock and 
 --have a stock quantity greater than 100. Select only their names and stock quantities.
 SELECT * FROM products;
--- Challenge 1.sql
+++
@@ -1,7 +0,0 @@
---Challenge 1: Find all products in the 'Accessories' category that cost more than $50, and 
---list them with the most expensive ones first.
-SELECT * FROM products;
-
-SELECT * FROM products
-WHERE category = 'Accessories' AND price > 50.00
-ORDER BY price DESC;
